4c5b7dc8103660a8850c9cf747e38da98ca00914
Yen Lin <lynn575938@outlook.com>
2023-06-29 18:01:08 +0800

    fix: fix small typo

--- app.js
+++ app.js
@@ -1,51 +1,51 @@
 const express = require('express')
 const exphbs = require('express-handlebars')
 const URL = require('./models/URL')
-const isUrlValid =require('./utils/isUrlValid')
+const isUrlValid = require('./utils/isUrlValid')
 const shortenURL = require('./utils/shortenURL')
 
 const app = express()
 
 require('./config/mongoose')
 
-app.engine('hbs', exphbs({defaultLayout: 'main', extname: '.hbs'}))
+app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
 app.set('view engine', 'hbs')
 
-app.use(express.urlencoded({ extended: true})) // 直接從 express 取用 body-parser
+app.use(express.urlencoded({ extended: true })) // 直接從 express 取用 body-parser
 
 app.get('/', (req, res) => {
   res.render('index')
 })
 
-app.post('/', async (req, res) => {
+app.post('/', (req, res) => {
   if (!req.body.url) return res.redirect('/')
 
   const originalURL = req.body.url
-  
+
   isUrlValid(originalURL)
     .then(() => {
       const shortURL = shortenURL(5)
-    
+
       URL.findOne({ originalURL })
-      .then((data) => {
-        // 確認有無資料，若有就直接取用現有網址，若無則在資料庫建立新資料
-        if (data) {
-          res.render('index', { origin: req.headers.origin, shortURL: data.shortURL })
-        } else {
-          URL.create({ originalURL, shortURL })
-            .then(() => {
-              res.render('index', { origin: req.headers.origin, shortURL })
-            })
-            .catch(err => console.error(err))
-        }
-      })
-      .catch(err => console.error(err))
+        .then((data) => {
+          // 確認有無資料，若有就直接取用現有網址，若無則在資料庫建立新資料
+          if (data) {
+            res.render('index', { origin: req.headers.origin, shortURL: data.shortURL })
+          } else {
+            URL.create({ originalURL, shortURL })
+              .then(() => {
+                res.render('index', { origin: req.headers.origin, shortURL })
+              })
+              .catch(err => console.error(err))
+          }
+        })
+        .catch(err => console.error(err))
 
     })
-  .catch(err => {
-    res.render('error')
-  })
-  
+    .catch(err => {
+      res.render('error')
+    })
+
 })
 
 app.get('/:shortURL', (req, res) => {
